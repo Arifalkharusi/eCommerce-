bbf304a8c0c3a7cc8dfd6799604d11310d0929f8
Arif <arifalkharus@hotmail.com>
2023-04-06 03:41:09 +0100

    new

---
+++ src/Components/ProductPage/ProductPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ViewListings from "../ViewListings/ViewListings";
+const ProductPage = (props) => {
+  return (
+    <div>
+      <ViewListings />
+    </div>
+  );
+};
+
+export default ProductPage;
