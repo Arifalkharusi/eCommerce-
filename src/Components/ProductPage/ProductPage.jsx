dfcfd166fa70f736a829773158239fd10fda686a
Arif <arifalkharus@hotmail.com>
2023-04-10 12:12:28 +0100

    10/04/23 2nd mod

--- src/Components/ProductPage/ProductPage.jsx
+++ src/Components/ProductPage/ProductPage.jsx
@@ -2,109 +2,15 @@ import React, { useState } from "react";
 import Product from "../Product/Product";
 import Listings from "../Listings/Listings";
 import style from "./ProductPage.module.css";
-import { useSelector } from "react-redux";
+import recomended from "../../data/recomended.json";
 
 const ProductPage = (props) => {
-  const { items } = useSelector((state) => state.dataSlice);
   const [move, setMove] = useState(0);
 
   const sliderHandler = (value) => {
     if (value === "left" && move > 0) setMove(move - 1);
     if (value === "right" && move < recomended.length - 5) setMove(move + 1);
   };
-
-  const recomended = [
-    {
-      id: 1,
-      name: "Oversized Half Sleeve Graphic T-shirt",
-      image:
-        "https://media.boohooman.com/i/boohooman/bmm34714_ecru_xl?$product_image_category_page_2x$&fmt=webp",
-      price: "12.60",
-      sizes: ["XS", "S", "M", "L", "XL", "XXL"],
-      color: ["Black", "Grey", "Navy", "Blue", "Brown"],
-    },
-    {
-      id: 2,
-      name: "Tall Oversized Half Sleeve Graphic T-shirt",
-      image:
-        "https://media.boohooman.com/i/boohooman/bmm36161_ecru_xl?$product_image_category_page_2x$&fmt=webp",
-      price: "12.60",
-      sizes: ["XS", "S", "M", "L", "XXL"],
-      color: ["Black", "Grey", "Navy", "Blue", "Brown"],
-    },
-    {
-      id: 3,
-      name: "Tall Oversized Faux Layer Embroidered T-sh/irt",
-      image:
-        "https://media.boohooman.com/i/boohooman/bmm36154_ecru_xl?$product_image_category_page_2x$&fmt=webp",
-      price: "12.60",
-      sizes: ["XS", "S", "M", "L", "XL"],
-      color: ["Black", "Grey", "Navy", "Blue", "Brown"],
-    },
-    {
-      id: 4,
-      name: "Oversized Back Graphic T-shirt",
-      image:
-        "https://media.boohooman.com/i/boohooman/bmm34716_ecru_xl?$product_image_category_page_2x$&fmt=webp",
-      price: "12.60",
-      sizes: ["XS", "S", "M", "L", "XL", "XXL"],
-      color: ["Black", "Grey", "Navy", "Blue", "Brown"],
-    },
-    {
-      id: 5,
-      name: "Oversized Half Sleeve Graphic T-shirt",
-      image:
-        "https://media.boohooman.com/i/boohooman/bmm34715_grey%20marl_xl?$product_image_category_page_2x$&fmt=webp",
-      price: "12.60",
-      sizes: ["XS", "S", "M", "L", "XL", "XXL"],
-      color: ["Black", "Grey", "Navy", "Blue", "Brown"],
-    },
-    {
-      id: 6,
-      name: "Tall Oversized Half Sleeve Graphic T-shirt",
-      image:
-        "https://media.boohooman.com/i/boohooman/bmm36155_grey%20marl_xl?$product_image_category_page_2x$&fmt=webp",
-      price: "12.60",
-      sizes: ["XS", "S", "L", "XL"],
-      color: ["Black", "Grey", "Navy", "Blue", "Brown"],
-    },
-    {
-      id: 7,
-      name: "Oversized Faux Layer Embroidered T-shirt",
-      image:
-        "https://media.boohooman.com/i/boohooman/bmm34721_grey%20marl_xl?$product_image_category_page_2x$&fmt=webp",
-      price: "12.60",
-      sizes: ["XS", "S", "M", "L", "XL", "XXL"],
-      color: ["Black", "Grey", "Navy", "Blue", "Brown"],
-    },
-    {
-      id: 8,
-      name: "Oversized Contrast Stitch Slogan T-shirt",
-      image:
-        "https://media.boohooman.com/i/boohooman/bmm34719_navy_xl?$product_image_category_page_2x$&fmt=webp",
-      price: "12.60",
-      sizes: ["XS", "S", "L", "XL"],
-      color: ["Black", "Grey", "Navy", "Blue", "Brown"],
-    },
-    {
-      id: 9,
-      name: "Oversized Faux Layer Embroidered T-shirt",
-      image:
-        "https://media.boohooman.com/i/boohooman/bmm34720_ecru_xl?$product_image_category_page_2x$&fmt=webp",
-      price: "12.60",
-      sizes: ["S", "M", "L", "XL", "XXL"],
-      color: ["Black", "Grey", "Navy", "Blue", "Brown"],
-    },
-    {
-      id: 10,
-      name: "Oversized Teddy Graphic Ringer T-shirt",
-      image:
-        "https://media.boohooman.com/i/boohooman/bmm34710_pink_xl?$product_image_category_page_2x$&fmt=webp",
-      price: "12.60",
-      sizes: ["XS", "M", "L", "XL", "XXL"],
-      color: ["Black", "Grey", "Navy", "Blue", "Brown"],
-    },
-  ];
 
   return (
     <div>
@@ -113,7 +19,7 @@ const ProductPage = (props) => {
         <div className={style.recomtitle}>WE THINK YOU'LL LIKE</div>
         <div className={style.recomline}></div>
       </div>
-      <div className={style.div}>
+      <div className={style.slider}>
         <button direction="left" onClick={() => sliderHandler("left")}>
           <i class="fa-solid fa-chevron-left"></i>
         </button>
